cc240f1c3fa73c5bc245229b80183eb1ae5621a1
Javier Chavez <javierchavezarce@gmail.com>
2019-08-24 17:38:54 -0400

    prueba 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-              CIESMORI
+                    LARAVEL CIESMORI
                 </div>
 
                 <div class="links">
