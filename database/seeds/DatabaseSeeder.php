e674bd75cac53e9b762a2b3cac5c8f8cf311940e
Javier Chavez <javierchavezarce@gmail.com>
2019-08-24 20:19:02 -0400

    seedings

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
     	$this->call(ParametricaGeneroTableSeeder::class);
+        $this->call(ParametricaCiudadTableSeeder::class);
+        $this->call(ParametricaDisponibilidadTiempoTableSeeder::class);
     	$this->call(ParametricaEstadoCivTableSeeder::class);
+        $this->call(ParametricaExpedidoTableSeeder::class);
+        $this->call(ParametricaHorarioDisponibleTableSeeder::class);
+        $this->call(ParametricaNivelEduTableSeeder::class);
+        $this->call(ParametricaTipoEstudioTableSeeder::class);
         $this->call(RolesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
     }
