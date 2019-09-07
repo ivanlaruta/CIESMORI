22857b33b9142ead6eb15aea872da0db122849d3
NathalyAlfaroOrtega <nathalyalfaroortega@gmail.com>
2019-09-07 00:24:48 -0400

    Realice el llenado del seed de la tabla posisiones_parametrica

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,9 +19,13 @@ class DatabaseSeeder extends Seeder
         $this->call(ParametricaHorarioDisponibleTableSeeder::class);
         $this->call(ParametricaNivelEduTableSeeder::class);
         $this->call(ParametricaTipoEstudioTableSeeder::class);
+        $this->call(ParametricaPosicionesSeeder::class);
         $this->call(RolesTableSeeder::class);
         $this->call(PersonaTableSeeder::class);
         $this->call(EncuestadorTableSeeder::class);
         $this->call(UsersTableSeeder::class);
+
+
+
     }
 }
