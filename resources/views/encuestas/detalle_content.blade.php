b4e73fc095423d96349cf629ba74cc2b787438a1
NathalyAlfaroOrtega <nathalyalfaroortega@gmail.com>
2019-10-16 12:38:16 -0400

    Estilo a la tabla de encuestas migradas

--- resources/views/encuestas/detalle_content.blade.php
+++ resources/views/encuestas/detalle_content.blade.php
@@ -1,5 +1,45 @@
+              <style type="text/css">
+
+                  thead,
+                  tbody {
+                  display: block;
+                  text-align: center;
+                  }
+
+                  thead {
+                  text-align: center;
+                  }
+
+                  tbody {
+                  background: white;
+                  overflow-y: auto;
+                  height: 60em;
+                  }
+
+                  thead th,
+                  tbody td {
+                  width: 4em;
+                  padding: 2px;
+                  }
+
+                  thead tr th:first-child,
+                  tbody tr td:first-child {
+                    width: 8em;
+                    min-width: 8em;
+                    max-width: 8em;
+                    word-break: break-all;
+                  }
+
+
+              </style>
+
+
+
+
+
+
                 <h4>Lista de encuesta / {{$encuesta_Cab->nombre}}</h4>
-                <table class="table table-bordered " id="tabla_filtrada">
+                <table class="table table-bordered table-responsive table-hover" id="tabla_filtrada">
                   <thead>
                     <tr>
                       @for  ($i = 0; $i < sizeof($array_campos); $i++)
@@ -16,7 +56,7 @@
                               echo $det->{$array_campos[$i]};
                           @endphp
                           </th>
-                         @endfor                        
+                         @endfor
                       </tr>
                     @endforeach
                   </tbody>
